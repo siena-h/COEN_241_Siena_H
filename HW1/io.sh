4b985f19a077c5da677e5fac152bb0233d2f2360
siena-h <shanna@scu.edu>
2023-02-01 21:59:08 -0800

    Updated io.sh and mode.sh

--- HW1/io.sh
+++ HW1/io.sh
@@ -1,62 +1,105 @@
 #Name: Siena Hanna
 #File: Test file for HW1 Experiments: fileio
 #Arguments: single argument, delay between tests in seconds (chosen for easy recording of results)
+#Run format: sh io.sh <delay>
 #Date: 01/29/23
-
+#Test Description: This runs mode.sh with all 6 fileio modes for file sizes of 256MB, 512MB, 1GB, and 2GB (five tests each) and creates files from each test for ease of documentation. 
 #!/bin/bash
 
 echo "SEQUENTIAL WRITE TEST\n"
 
-sh mode.sh seqwr 1G
-sleep $1
-sh mode.sh seqwr 2G
-sleep $1
-sh mode.sh seqwr 4G
-sleep $1
+i=0
+inc=1
+iter=5
+while [ $i -lt $iter ]
+do
+	sh mode.sh seqwr 1G > seqwr/seqwr1G$i.txt
+	sleep $1
+	sh mode.sh seqwr 2G > seqwr/seqwr2G$i.txt
+	sleep $1
+	sh mode.sh seqwr 256M > seqwr/seqwr256M$i.txt
+	sleep $1
+	sh mode.sh seqwr 512M > seqwr/seqwr512M$i.txt
+	i=$(($i+$inc))
+	sleep $1
+done
 
 echo "SEQUENTIAL REWRITE TEST\n"
 
-sh mode.sh seqrewr 1G
-sleep $1
-sh mode.sh seqrewr 2G
-sleep $1
-sh mode.sh seqrewr 4G
-sleep $1
+i=0
+while [ $i -lt $iter ]
+do
+	sh mode.sh seqrewr 1G > seqrewr/seqrewr1G$i.txt
+	sleep $1
+	sh mode.sh seqrewr 2G > seqrewr/seqrewr2G$i.txt
+	sleep $1
+	sh mode.sh seqrewr 256M > seqrewr/seqrewr256M$i.txt
+	sleep $1
+	sh mode.sh seqrewr 512M > seqrewr/seqrewr512M$i.txt
+	sleep $1
+	i=$(($i+$inc))
+done
 
 echo "SEQUENTIAL READ TEST\n"
 
-sh mode.sh seqrd 1G
-sleep $1
-sh mode.sh seqrd 2G
-sleep $1
-sh mode.sh seqrd 4G
-sleep $1
+i=0
+while [ $i -lt $iter ]
+do
+	sh mode.sh seqrd 1G > seqrd/seqrd1G$i.txt
+	sleep $1
+	sh mode.sh seqrd 2G > seqrd/seqrd2G$i.txt
+	sleep $1
+	sh mode.sh seqrd 256M > seqrd/seqrd256M$i.txt
+	sleep $1
+	sh mode.sh seqrd 512M > seqrd/seqrd512M$i.txt
+	sleep $1
+	i=$(($i+$inc))
+done
 
 echo "RANDOM READ TEST\n"
 
-sh mode.sh rndrd 1G
-sleep $1
-sh mode.sh rndrd 2G
-sleep $1
-sh mode.sh rndrd 4G
-sleep $1
-
+i=0
+while [ $i -lt $iter ]
+do
+	sh mode.sh rndrd 1G > rndrd/rndrd1G$i.txt
+	sleep $1
+	sh mode.sh rndrd 2G > rndrd/rndrd2G$i.txt
+	sleep $1
+	sh mode.sh rndrd 256M > rndrd/rndrd256M$i.txt
+	sleep $1
+	sh mode.sh rndrd 512M > rndrd/rndrd512M$i.txt
+	sleep $1
+	i=$(($i+$inc))
+done
 echo "RANDOM WRITE TEST\n"
 
-sh mode.sh rndwr 1G
-sleep $1
-sh mode.sh rndwr 2G
-sleep $1
-sh mode.sh rndwr 4G
-sleep $1
+i=0
+while [ $i -lt $iter ]
+do
+	sh mode.sh rndwr 1G > rndwr/rndwr1G$i.txt
+	sleep $1
+	sh mode.sh rndwr 2G > rndwr/rndwr2G$i.txt
+	sleep $1
+	sh mode.sh rndwr 256M > rndwr/rndwr256M$i.txt
+	sleep $1
+	sh mode.sh rndwr 512M > rndwr/rndwr512M$i.txt
+	sleep $1
+	i=$(($i+$inc))
+done
 
 echo "COMBINED RANDOM READ/WRITE TEST\n"
 
-sh mode.sh rndrw 1G
-sleep $1
-sh mode.sh rndrw 2G
-sleep $1
-sh mode.sh rndrw 4G
-sleep $1
-
+i=0
+while [ $i -lt $iter ]
+do
+	sh mode.sh rndwr 1G > rndrw/rndrw1G$i.txt
+	sleep $1
+	sh mode.sh rndrw 2G > rndrw/rndrw2G$i.txt
+	sleep $1
+	sh mode.sh rndrw 256M > rndrw/rndrw256M$i.txt
+	sleep $1
+	sh mode.sh rndrw 512M > rndrw/rndrw512M$i.txt
+	sleep $1
+	i=$(($i+$inc))
+done
 
--- HW1/mode.sh
+++ HW1/mode.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #Siena Hanna
-#Shell program for fileio tests. First argument to sh io.sh is the test mode (ex: rndrw), second argument is size (ex: "1G")
-#call formatting: sh mode.sh <fileio mode> <total file size>
-
+#Shell program for fileio tests. First argument to sh mode.sh is the test mode (ex: rndrw), second argument is total file size (ex: "1G")
+#formatting: sh mode.sh <fileio mode> <total file size>
+#Time set to 2 mins maximum (120 seconds). I am running 3 tests per total file size for 5 file sizes on 6 modes (for three scenarios).
 
 echo "TEST START: $2, default file number (128 Files)"
 
 sysbench --test=fileio --file-total-size=$2 --file-test-mode=$1 prepare
 
-sysbench --test=fileio --file-total-size=$2 --file-test-mode=$1 run
+sysbench --test=fileio --max-time=180 --file-total-size=$2 --file-test-mode=$1 run
 
 sleep 5
 
 sysbench --test=fileio --file-total-size=$2 --file-test-mode=$1 cleanup
 
 f=1
-inc=4 #file number quadruples each iteration, up to 256
-while [ $f -lt 256 ]
+inc=16 #file size increases by a factor of 16
+while [ $f -le 256 ]
 do
 	sleep 10 #so I can run sync on my computer to clear cache between tests
 	echo "$2, $f file(s)\n"
 	sysbench --test=fileio --file-total-size=$2 --file-num=$f --file-test-mode=$1 prepare
-	sysbench --test=fileio --file-total-size=$2 --file-num=$f --file-test-mode=$1 run
+	sysbench --test=fileio --max-time=180 --file-total-size=$2 --file-num=$f --file-test-mode=$1 run
 	sleep 5
 	sysbench --test=fileio --file-total-size=$2 --file-num=$f --file-test-mode=$1 cleanup
 
